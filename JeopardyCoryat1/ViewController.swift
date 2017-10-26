41cf86f6b275e0079bd8c9a731c651b8bad8ce4c
Frain <frainm@1101C202255LM.local>
2017-10-26 17:15:43 -0400

    Project generation

---
+++ JeopardyCoryat1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JeopardyCoryat1
+//
+//  Created by Frain, Michael F. on 10/26/17.
+//  Copyright © 2017 Michael Frain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
